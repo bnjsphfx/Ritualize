6700ecdf6d2b92e5d3a3bbb124de29639907a186
Ben Fox <benjosephfox@gmail.com>
2022-06-16 22:04:19 +0100

    Initial Commit

---
+++ Ritualize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ritualize
+//
+//  Created by Ben Fox on 16/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ritualize/RitualizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitualizeApp.swift
+//  Ritualize
+//
+//  Created by Ben Fox on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RitualizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
